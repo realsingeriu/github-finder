f555afcf8814aa45b051e934680dc52ec6008305
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-30 11:52:15 +0900

    spinner 추가 및 설정 완료

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 
+import Spinner from "../layout/Spinner";
+
 function UserResults() {
   const [users, setUsers] = useState([]);
+  const [loaing, setLoading] = useState(true); // 시작시 깃허브에서 데이터를 자겨옴(작업중 true)
   useEffect(() => {
     fetchUsers();
   }, []); //앱 시작시 실행됨
@@ -16,14 +19,19 @@ function UserResults() {
     const data = await response.json();
 
     setUsers(data);
+    setLoading(false); // 데이터 로딩 완료
   };
-  return (
-    <div className="grid grid-cols-1 gap-8 md:gird-cols-2 xl:grid-cols-4 lg:grid-cols-3">
-      {users.map((user) => (
-        <h3>{user.login}</h3>
-      ))}
-    </div>
-  );
+  if (!loaing) {
+    return (
+      <div className="grid grid-cols-1 gap-8 md:gird-cols-2 xl:grid-cols-4 lg:grid-cols-3">
+        {users.map((user) => (
+          <h3 key={user.id}>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
 }
 
 export default UserResults;
