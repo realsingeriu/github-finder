525cbe1414998d480f231ca6e3ef9381b8b4a05d
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-30 10:21:41 +0900

    페이지 라우트 설정 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 import React from "react";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -9,7 +12,14 @@ function App() {
     <BrowserRouter>
       <div className="flex flex-col justify-between h-screen">
         <Navbar title="깃허브" />
-        <main>Content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </BrowserRouter>
