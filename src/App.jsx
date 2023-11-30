8474d0d3f0bbf55ed3452f65b12b4dc0bb33c930
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-30 14:24:15 +0900

    클로벌 깃허브콘텍스트

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,24 @@ import React from "react";
 import NotFound from "./pages/NotFound";
 import About from "./pages/About";
 import Home from "./pages/Home";
-import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
-    <GithubProvider>
-      {/* 링크가 안맞아서 오류라던지 basename속성은 null 이다라는 오류가 나오면
-      'BrowserRouter'를 쓰면 해결 된다. 즉, 속성을 구조화하는데 문제가 생긴거다. */}
-      <BrowserRouter>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar title="깃허브" />
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/notfound" element={<NotFound />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </BrowserRouter>
-    </GithubProvider>
+    // 링크가 안맞아서 오류라던지 basename속성은 null 이다라는 오류가 나오면'BrowserRouter'를 쓰면 해결 된다. 즉, 속성을 구조화하는데 문제가 생긴거다.
+    <BrowserRouter>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar title="깃허브" />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
