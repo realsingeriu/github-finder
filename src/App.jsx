d6735fee44ccee5d96a654c0d1e958db2ffcf9ef
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-30 10:02:34 +0900

    네브바, 푸터 추가 및 설정 , About,Home,NotFound추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter } from "react-router-dom";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+import React from "react";
+
 function App() {
   return (
-    <div>
-      <h1>헬로우 월드!</h1>
-    </div>
+    // 링크가 안맞아서 오류라던지 basename속성은 null 이다라는 오류가 나오면  'BrowserRouter'를 쓰면 해결 된다. 즉, 속성을 구조화하는데 문제가 생긴거다.
+    <BrowserRouter>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar title="깃허브" />
+        <main>Content</main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
