e2376db0a7d3298c77c6b8c739d62b6ef9923d82
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-01 10:16:30 +0900

    User에 param 설정해서 해당 유저 정보를 가져올 수 있게 설정

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,5 +1,20 @@
-function User() {
-  return <div>유저 페이지</div>;
-}
+import { useParams } from "react-router-dom";
+import GithubContext from "../context/github/GithubContext";
+import { useContext } from "react";
 
-export default User;
+function User() {
+  const { user, getUser } = useContext(GithubContext);
+
+  const params = useParams();
+
+  useEffect(() => {
+    getUser(params.login);
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <h3>
+      {user.login}, {user.id}
+    </h3>
+  );
+}
