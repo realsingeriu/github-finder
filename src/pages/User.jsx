530158c73b9dbdfedde5868d2b25e0fb1df7226d
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-01 11:03:53 +0900

    유저 페이지 (위치,블로그,트위터 등) 추가

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -2,6 +2,7 @@ import { Link, useParams } from "react-router-dom";
 import GithubContext from "../context/github/GithubContext";
 import { useContext, useEffect } from "react";
 import Spinner from "../components/layout/Spinner";
+import { FaCodepen, FaUserFriends, FaUsers } from "react-icons/fa";
 
 function User() {
   const { user, getUser, loading } = useContext(GithubContext);
@@ -74,6 +75,77 @@ function User() {
                 >
                   Visit Github Profile
                 </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <hr></hr>
+        </div>
+        <div className="w-full rounded-lg shadow-md bg-base-100 stats">
+          {location && (
+            <div className="stat">
+              <div className="stat-title text-md">Location</div>
+              <div className="text-lg stat-value">{location}</div>
+            </div>
+          )}
+          {blog && (
+            <div className="stat">
+              <div className="stat-title text-md">Website</div>
+              <div className="text-lg stat-value">
+                <a href={blog} target="_blank" rel="noreferrer">
+                  {blog}
+                </a>
+              </div>
+            </div>
+          )}
+          {twitter_username && (
+            <div className="stat">
+              <div className="stat-title text-md">Twitter</div>
+              <div className="text-lg stat-value">
+                <a
+                  href={`https://twitter.com/${twitter_username}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {twitter_username}
+                </a>
+              </div>
+            </div>
+          )}
+        </div>
+        <div>
+          <hr></hr>
+        </div>
+        <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
+          <div className="grid grid-cols-1 md:grid-cols-3">
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaUsers className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Followers</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {followers}
+              </div>
+            </div>
+
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaUserFriends className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Following</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {following}
+              </div>
+            </div>
+
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaCodepen className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Public Repos</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {public_repos}
               </div>
             </div>
           </div>
