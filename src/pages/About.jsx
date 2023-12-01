12f92ad5096079eb5a3bf242a851c59da8620b12
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-12-01 16:38:46 +0900

    about 수정

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,66 @@
 function About() {
   return (
-    <>
-      <h1 className="text-6xl mb-4">Github Finder</h1>
-      <p className="mb-4 text-2xl font-light">
-        깃허브 사용자를 찾고 프로파일을 확인할 수 있는 리엑트 앱입니다. 나의
-        블로그 주소는 다음과 같습니다.
-        <strong>
-          <a href="https://blog.naver.com/drv983"> 내 블로그</a>
-        </strong>
-        .
-      </p>
-      <p className="text-lg text-gray-400">
-        Version <span> 1.0.0</span>
-      </p>
-    </>
+    <div className="flex max-w-2xl mx-auto mt-10 p-6 bg-white rounded-lg shadow-xl">
+      <div className="w-3/4">
+        <h1 className="text-6xl mb-4 font-bold text-gray-800">Github Finder</h1>
+        <p className="mb-4 text-2xl font-light text-gray-600">
+          깃허브 사용자를 찾고 프로파일을 확인할 수 있는 리액트 앱입니다. 참고
+          할만한 블로그 주소 링크
+        </p>
+        <p className="text-lg text-gray-400 mb-4">
+          Version <span className="font-bold">1.0.0</span>
+        </p>
+        <ul className="list-inside list-disc mb-4">
+          <li>
+            <i>
+              <strong>
+                <a
+                  href="https://blog.naver.com/drv983"
+                  className="text-blue-500 hover:underline"
+                >
+                  jbkim 블로그 링크
+                </a>
+              </strong>
+            </i>
+          </li>
+          <li>
+            <i>
+              <strong>
+                <a
+                  href="https://blog.naver.com/coding_ori"
+                  className="text-blue-500 hover:underline"
+                >
+                  keangEE 블로그 링크
+                </a>
+              </strong>
+            </i>
+          </li>
+          <li>
+            <i>
+              <strong>
+                <a
+                  href="https://blog.naver.com/realsingeriu12"
+                  className="text-blue-500 hover:underline"
+                >
+                  realsingeriu 블로그 링크
+                </a>
+              </strong>
+            </i>
+          </li>
+        </ul>
+        <div>
+          <p className="text-gray-600">
+            <strong>Contact Me:</strong>{" "}
+            <a
+              href="mailto:realsingeriu12@naver.com"
+              className="text-blue-500 hover:underline"
+            >
+              <spnan>realsingeriu12@naver.com</spnan>
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
